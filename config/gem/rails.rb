e64853564b6cade78fbe046558b1e28909906ea7
nautilus@narwhal <nautilus@trovaprezzi.it>
2021-12-09 10:18:19 +0100

    Commenti.

--- config/gem/rails.rb
+++ config/gem/rails.rb
@@ -2,17 +2,17 @@ class Gem::Rails
 
   def self.production(config)
     config.cache_classes = true
-    config.eager_load = true
+    config.eager_load = true #on boot, in pro per performare meglio subito
   end
 
   def self.development(config)
     config.cache_classes = false
-    config.eager_load = false
+    config.eager_load = false #on boot
   end
 
   def self.test(config)
     config.cache_classes = true
-    config.eager_load = false
+    config.eager_load = false #on boot, potrebbe non servire per un singolo test
   end
 
 end
